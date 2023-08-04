523703a5fff0ad92caffa1acf298467aef5ee251
JoshmPerry <108098612+JoshmPerry@users.noreply.github.com>
2023-08-03 19:51:32 -0500

    modified:   index.html 	modified:   interface.js 	modified:   style.css

--- interface.js
+++ interface.js
@@ -1,7 +1,37 @@
-$("#title").html("Back for more");
-$(".Sections:even").css("background-color","#d7dee4");
+$(document).ready(new function(){
+    $(".Sections:even").css("background-color","#d7dee4");
+    $(".Sections").each(function(index){
+        clicker(this);
+    });
 
-function clicker(){
-    console.log("activated");
+});
+
+
+
+
+
+function clicker(thing){
+    if($(thing).attr("id")=="Projects"){
+        if($(thing).attr("expanded")=="false"){
+            $(thing).html(projectHTML());
+            $(thing).attr("expanded","true");
+        }else{
+            $(thing).html(projectTitle());
+            $(thing).attr("expanded","false");
+        }
+    }
 }
 
+function projectHTML(){
+    var html="";
+    html+="<table style='margin-left:auto;margin-right:auto'><tr><th><h1>Projects</h1></th><td>Section Information</td></tr></table>";
+
+    return html;
+}
+
+function projectTitle(){
+    var html="";
+    html+="<h1>Projects</h1>";
+
+    return html;
+}
