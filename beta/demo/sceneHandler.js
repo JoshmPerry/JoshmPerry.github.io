c60299d5db7f2f8fd3ce2e5d3e040eb37c922e74
JoshmPerry <108098612+JoshmPerry@users.noreply.github.com>
2024-01-11 18:53:11 -0600

    This movement stuff is going to be super fun D= 	modified:   beta/demo/index.html 	modified:   beta/demo/sceneHandler.js

--- beta/demo/sceneHandler.js
+++ beta/demo/sceneHandler.js
@@ -2,13 +2,13 @@ zoomed=false;
 sensitivity=0.01;
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75,640/480,.1,1000);
+const camera = new THREE.PerspectiveCamera(75,document.querySelector('#glcanvas').width/document.querySelector('#glcanvas').height,.1,1000);
 
 
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#glcanvas'),
 });
-
+renderer.setSize(document.querySelector('#glcanvas').width,document.querySelector('#glcanvas').height);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.shadowMap.enabled = true;
     		renderer.shadowMap.type = THREE.BasicShadowMap;
