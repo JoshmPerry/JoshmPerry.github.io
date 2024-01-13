a834318246764e65ab238caea637c05264199b75
JoshmPerry <108098612+JoshmPerry@users.noreply.github.com>
2024-01-13 16:37:30 -0600

    What a crazy rabbit hole, demo is working 	modified:   beta/demo/index.html 	modified:   beta/demo/sceneHandler.js

--- beta/demo/sceneHandler.js
+++ beta/demo/sceneHandler.js
@@ -2,8 +2,8 @@ zoomed=false;
 sensitivity=0.01;
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75,document.querySelector('#glcanvas').width/document.querySelector('#glcanvas').height,.1,1000);
-
+camera = new THREE.PerspectiveCamera(75,document.querySelector('#glcanvas').width/document.querySelector('#glcanvas').height,.1,1000);
+camera.rotation.order="ZYX";
 
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#glcanvas'),
@@ -64,25 +64,22 @@ document.querySelector('#glcanvas').addEventListener("click", async () => {
   function logMovement(event) {
     if(zoomed){
         console.log("camera",camera.rotation.x,", ",camera.rotation.y);
-
-    camera.rotation.y-=event.movementX*sensitivity;
+    if(camera.rotation.x-event.movementY*sensitivity<Math.PI/2 && camera.rotation.x-event.movementY*sensitivity>-Math.PI/2)
     camera.rotation.x-=event.movementY*sensitivity;
+    camera.rotation.y-=event.movementX*sensitivity;
+
     }
   }
 
   function relationalAngle(){
-
+    diq = new Quaternion();
   }
+  diq = new Quaternion();
 
   function logScroll(event){
     if(zoomed){
-        console.log("camera",camera.rotation,event.wheelDelta);
-        console.log(camera.rotation.x%(Math.PI*2));
-        console.log("x",Math.asin(camera.rotation.x%1));
-        console.log("y",Math.asin(camera.rotation.y%1));
-        console.log("True X",Math.sin(camera.rotation.x/(Math.PI*2))*Math.acos(camera.rotation.y/(Math.PI*2)%1));
         camera.position.x-=Math.sin(camera.rotation.y)*Math.cos(camera.rotation.x)*event.wheelDelta/5;
-        camera.position.y+=Math.cos(camera.rotation.y)*Math.sin(camera.rotation.x)*event.wheelDelta/5;
+        camera.position.y+=Math.cos(camera.rotation.y%(Math.PI/2))*Math.sin(camera.rotation.x)*event.wheelDelta/5;
         camera.position.z-=Math.cos(camera.rotation.y)*Math.cos(camera.rotation.x)*event.wheelDelta/5;
     }
   }
@@ -96,3 +93,12 @@ document.querySelector('#glcanvas').addEventListener("click", async () => {
   document.addEventListener("pointerlockchange", async () => {
     zoomed=!zoomed;
   });
+
+  window.onresize=() =>{
+    console.log("hi")
+    document.querySelector("#glcanvas").setAttribute("width",(window.innerWidth) + "px");
+    document.querySelector("#glcanvas").setAttribute("height",(window.innerHeight) + "px");
+    renderer.setSize(document.querySelector('#glcanvas').width,document.querySelector('#glcanvas').height);
+    camera.aspect=document.querySelector('#glcanvas').width/document.querySelector('#glcanvas').height;
+    camera.updateProjectionMatrix();
+  }
